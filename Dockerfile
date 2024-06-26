3dd850f390eca461d25b44a2d626b9536095437d
serratos <serratoser@globalhitss.com>
2024-06-26 10:34:28 -0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:14-alpine as build-step
+#FROM node:14 as build-step
+    RUN mkdir -p /app
+    WORKDIR /app
+    COPY package.json /app
+    RUN npm install
+    RUN npm install -g npm@7.20.5
+    COPY . /app
+    RUN npm run build --prod
+
+FROM nginx:latest
+COPY --from=build-step /app/dist/myApp /usr/share/nginx/html
+COPY nginx.conf /etc/nginx/conf.d/default.conf
---
+++ kuberployder.sh
@@ -0,0 +1,27 @@
+pwd
+ls
+echo "Trying to launche the pipeline"
+#cd conf
+#mvn clean install
+#mvn package spring-boot:repackage
+if [ "$?" -eq "1" ]
+then
+  echo "fallo"
+  exit 1
+fi
+imageRepository="10.168.131.38:5000"
+projectName="my-app"
+
+docker build . -t "$imageRepository/$projectName"
+docker push "$imageRepository/$projectName"
+
+kubectl delete -f deploy.yaml && true
+kubectl apply -f deploy.yaml
+
+nService=$(kubectl get services | grep "$projectName-service" | wc -l)
+
+if [ $nService -lt 1 ]
+then
+  kubectl expose deployment $projectName --type=LoadBalancer --name="$projectName-service" && true
+fi
+echo $(kubectl get services | grep "$projectName-service")
